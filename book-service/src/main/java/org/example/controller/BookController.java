a0289d9be8e7cb56e8047599eaac223b8f02fa02
Lmming <2642253902@qq.com>
2026-06-07 16:08:36 +0800

    6.7

--- book-service/src/main/java/org/example/controller/BookController.java
+++ book-service/src/main/java/org/example/controller/BookController.java
@@ -1,6 +1,8 @@
 package org.example.controller;
 
 import jakarta.annotation.Resource;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import org.example.entiey.Book;
 import org.example.service.BookService;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +16,9 @@ public class BookController {
     BookService service;
 
     @RequestMapping("/book/{bid}")
-    Book findBookById(@PathVariable("bid") int bid) {
+    Book findBookById(@PathVariable("bid") int bid, HttpServletRequest request) {
+        String test = request.getHeader("Test");
+        System.out.println("Test: " + test);
         return service.getBookById(bid);
     }
 }
