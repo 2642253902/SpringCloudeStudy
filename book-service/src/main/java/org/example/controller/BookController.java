a2093a7004cb4a19164753157dd141ea9ed6db51
Lmming <2642253902@qq.com>
2026-07-16 11:42:59 +0800

    7.16

--- book-service/src/main/java/org/example/controller/BookController.java
+++ book-service/src/main/java/org/example/controller/BookController.java
@@ -1,13 +1,17 @@
 package org.example.controller;
 
+import jdk.nashorn.internal.ir.annotations.Reference;
 import org.example.entiey.Book;
 import org.example.service.BookService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@RefreshScope
 @RestController
 public class BookController {
 
@@ -18,4 +22,12 @@ public class BookController {
     Book findBookById(@PathVariable("bid") int bid) {
         return service.getBookById(bid);
     }
+    @Value("${test.name}")
+    String name;
+
+    @RequestMapping("/test")
+    String  test() {
+        return name;
+    }
+
 }
